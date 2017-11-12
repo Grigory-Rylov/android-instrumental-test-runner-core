2ac908dd7cb9c26d2ed22efb844674c507482385
Grigory Rylov <grishberg@gmail.com>
2017-11-12 19:50:40 +0300

    remove redundant classes

--- src/main/java/log/Logger.java
+++
@@ -1,9 +0,0 @@
-package log;
-
-/**
- * Created by grishberg on 07.09.17.
- */
-public interface Logger {
-    void log(String message);
-    void log(String format, String ... values);
-}
--- src/main/java/tasks/Task.java
+++
@@ -1,8 +0,0 @@
-package tasks;
-
-/**
- * Created by grishberg on 05.09.17.
- */
-public interface Task {
-    void execute();
-}
