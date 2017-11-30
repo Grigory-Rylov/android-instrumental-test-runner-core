32d46a2c0b0ec513080fc46472e21cbee9b93678
Grigory Rylov <grishberg@gmail.com>
2017-11-30 22:25:56 +0300

    added sonar and travis

--- src/main/java/com/github/grishberg/tests/InstrumentationArgsProvider.java
+++ src/main/java/com/github/grishberg/tests/InstrumentationArgsProvider.java
@@ -3,8 +3,12 @@ package com.github.grishberg.tests;
 import java.util.Map;
 
 /**
- * Created by grishberg on 20.10.17.
+ * Provides arguments for starting target device.
  */
 public interface InstrumentationArgsProvider {
+    /**
+     * @param targetDevice The target device on which tests will be run.
+     * @return map of arguments
+     */
     Map<String, String> provideInstrumentationArgs(DeviceWrapper targetDevice);
 }
