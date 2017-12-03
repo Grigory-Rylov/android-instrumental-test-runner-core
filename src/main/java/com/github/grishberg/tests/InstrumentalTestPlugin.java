94d62a5db699144ed6097e97966884c3b40fd451
Grigory Rylov <grishberg@gmail.com>
2017-12-03 09:37:04 +0300

    Plugin in java

---
+++ src/main/java/com/github/grishberg/tests/InstrumentalTestPlugin.java
@@ -0,0 +1,19 @@
+package com.github.grishberg.tests;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+
+/**
+ * Plugin for testing.
+ */
+public class InstrumentalTestPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.getLogger().info("InstrumentalTestPlugin applied");
+        Task task = project.getTasks()
+                .create(InstrumentalTestTask.TASK_NAME, InstrumentalTestTask.class);
+        task.setGroup("verification");
+        task.setDescription("Plugin for running instrumental tests on multiple devices");
+    }
+}
