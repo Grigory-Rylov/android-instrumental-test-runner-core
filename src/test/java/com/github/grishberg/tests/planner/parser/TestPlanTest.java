2dc754e4b7d4933f0fbdab4a9f8fa38d2dfe20a1
Grigory Rylov <grishberg@gmail.com>
2018-02-27 08:14:57 +0300

    added test for test plan

---
+++ src/test/java/com/github/grishberg/tests/planner/parser/TestPlanTest.java
@@ -0,0 +1,24 @@
+package com.github.grishberg.tests.planner.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by grishberg on 27.02.18.
+ */
+@RunWith(JUnit4.class)
+public class TestPlanTest {
+    private static final String[] NOT_EMPTY_ANNOTATIONS = {"Annotation1", "Annotation2"};
+
+    @Test
+    public void whenPutNullAnnotation_annotationsCleared() {
+        TestPlan plan = new TestPlan("", "", "");
+        plan.setAnnotations(NOT_EMPTY_ANNOTATIONS);
+
+        plan.setAnnotations(null);
+
+        Assert.assertNull(plan.getAnnotations());
+    }
+}
